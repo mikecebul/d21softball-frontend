21e8c58eb84fbe08c747a429562c2bac09b1ecde
Mike Cebulski <mikecebul@gmail.com>
2021-08-02 00:00:33 -0400

    Add new server IP

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://mikecebul.cloud:1337";
+  process.env.NEXT_PUBLIC_API_URL || "167.99.54.110";
 
 /**
  * Given an image return the Url
