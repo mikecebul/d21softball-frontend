1e6caa2c42ed5d29c9d5c71a0d9a80a6666103fb
mikecebul <mikecebul@gmail.com>
2021-08-11 15:31:20 -0400

    fix api url

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "localhost:1337";
+  process.env.NEXT_PUBLIC_API_URL || "https://api.mikecebul.cloud";
 
 /**
  * Given an image return the Url
