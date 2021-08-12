97bb78470f2ea28e513a394eff4ef5ad6f55231b
Mikecebul <mikecebul@gmail.com>
2021-08-11 20:14:13 -0400

    cover more with AuthProvider

--- pages/_app.js
+++ pages/_app.js
@@ -20,22 +20,22 @@ export default function MyApp(props) {
 
   return (
     <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <AuthProvider>
+      <AuthProvider>
+        <Head>
+          <title>My page</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
           <Layout>
             <Component {...pageProps} />
           </Layout>
-        </AuthProvider>
-      </ThemeProvider>
+        </ThemeProvider>
+      </AuthProvider>
     </React.Fragment>
   );
 }
