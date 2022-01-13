10c9f2f1bac8fdf4c145e8d2c1ac71738357ee3f
mikecebul <mikecebul@gmail.com>
2022-01-13 11:53:15 -0500

    Update Copyright name

--- src/Copyright.js
+++ src/Copyright.js
@@ -7,7 +7,7 @@ export default function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="/">
-        MASA D21
+        D21 Softball
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
