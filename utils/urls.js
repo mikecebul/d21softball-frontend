55a1ffb35646720660a2c141601b40745cdca186
mikecebul <mikecebul@gmail.com>
2022-01-13 16:41:07 -0500

    fix stripe key attempt 5

--- utils/urls.js
+++ utils/urls.js
@@ -1,7 +1,8 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
-export const STRIPE_PK = process.env.NEXT_STRIPE_PK || process.env.stripePK;
+export const STRIPE_PK =
+  process.env.NEXT_PUBLIC_STRIPE_PK || process.env.stripePK;
 
 /**
  * Given an image return the Url
