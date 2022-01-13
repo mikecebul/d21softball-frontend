6032679da568b351d3adab3ef493513bc60ce0d0
mikecebul <mikecebul@gmail.com>
2022-01-13 16:43:21 -0500

    remove console.log stripe key

--- components/BuyButton.js
+++ components/BuyButton.js
@@ -41,7 +41,6 @@ export default function BuyButton({ tournament, camp, selected }) {
       });
   };
 
-  console.log("Stipe Public Key:", STRIPE_PK);
   return (
     <>
       {!user.isAuthenticated && (
