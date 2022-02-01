583aaad1b9c1586d11bfd32ded1fbd92a9c10c03
Mikecebul <mikecebul@gmail.com>
2022-02-01 01:55:24 -0500

    Fill background behind Index hero image

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
-    backgroundColor: theme.palette.background.paper,
+    // backgroundColor: theme.palette.background.paper,
     // padding: theme.spacing(8, 4, 8, 4),
     // [theme.breakpoints.down("xs")]: {
     //   padding: theme.spacing(4, 4, 4, 4),
